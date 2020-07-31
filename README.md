ed0fce245ba88f9cb6b31b5eb0db685ae0989327
Gabriel Almir <avlye@outlook.com>
2020-07-31 03:06:59 -0300

    Add: initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# AppImage para Cisco Packet Tracer
+
+*🎶I'm already Tracer🎶*
+
+A configuration tool for building appimages with [pkg2appimage](https://github.com/AppImage/pkg2appimage) for Packet Tracer.
+This repo is a fork from [Diolinux/PacketTracer-AppImage](https://github.com/Diolinux/PacketTracer-AppImage).
+
+## Motivations
+
+Would be ideal having an automated build release for Cisco Packet Tracer, so you can always get the latest release.
+Thinking about that, I decided to create this repo as an opinionated tool for building app images.
+
+## How to contribute?
+
+### Requirements
+- git
+- wget
+
+### Get Started
+
+1. Clone this repo
+  ```shell
+  git clone https://github.com/Avlye/PacketTracer-AppImage
+  cd PacketTracer-AppImage/
+  ```
+
+2. Download pkg2appimage tool and create a executable binary
+  ```shell
+   wget https://github.com/AppImage/pkg2appimage/raw/master/pkg2appimage
+   chmod +x pkg2appimage
+   ```
+3. Build
+  ```shell
+   ./pkg2appimage PacketTracer.yml
+   ```
+
+4. After all this process, the appimage of Packet Tracer will be in `out/` directory.
+
+5. To run the application, just click on the AppImage, just like any other AppImage.
+  Also check if AppImage has permissions to execute the file properties. (**Right click on >> Properties**)
---
+++ src/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import sys
+from pathlib import Path
+
+
+class Tracer:
+    def __init__(self):
+        self.HOME = str(Path.home())
+        self.instalation_path = Path.joinpath(self.HOME, "PacketTracer")
+
+        self.current_directory = Path.cwd()
+        self.settings_path = Path.joinpath(self.HOME, ".ptappimage00")
+
+    def recall(self):
+        with open(self.settings_path, "wb") as settings_file:
+            file_header = bytearray([0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+            path_start = bytearray(">", "utf16")[2:]
+            path_separator = bytearray([0x00, 0x00, 0x00, 0x32, 0x00])
+
+            apppath = Path.joinpath(self.current_directory, "opt", "pt")
+            apppath_saves = Path.joinpath(apppath, "saves")
+
+            Path(apppath).mkdir(parents=True, exist_ok=True)
+            Path(apppath_saves).mkdir(parents=True, exist_ok=True)
+
+            apppath_encoded = bytearray(apppath, "utf16")[2:-1]
+
+            instalation_path_enconded = bytearray(self.instalation_path, "utf16")[2:-1]
+
+            encodedPaths = (
+                path_start
+                + apppath_encoded
+                + path_separator
+                + instalation_path_enconded
+            )
+
+            bytes_to_write = file_header + encodedPaths
+            settings_file.write(bytes_to_write)
+
+
+if __name__ == "__main__":
+    tracer = Tracer()
+    tracer.recall()
